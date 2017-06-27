4f7c279dd1b8bc9291cbaec58d5147daac018cd9
Veaceslav Iachim <veaceslav.iachim@amdaris.com>
2017-06-27 15:14:06 +0300

    Cinci

--- test/test/Program.cs
+++ test/test/Program.cs
@@ -10,13 +10,8 @@ namespace test
     {
         static void Main(string[] args)
         {
-            string FirstName = "Veaceslav";
-            string MentorName = "Marian";
-            Console.WriteLine("Buna ziua");
-            Console.WriteLine("{0}", FirstName);
-            Console.WriteLine("Bine ati venit la Amdaris");
-            Console.WriteLine("Mentorul tau este {0}", MentorName);
-            Console.WriteLine("In caz de necesitate va adresati dumnealui");
-         }
+
+            Console.WriteLine("Salutare");
+        }   
     }
 }
