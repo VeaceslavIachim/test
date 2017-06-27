969b3c16f6140812e660b41518ff868e4b0f6126
Veaceslav Iachim <veaceslav.iachim@amdaris.com>
2017-06-27 14:40:51 +0300

    Third commit

--- test/test/Program.cs
+++ test/test/Program.cs
@@ -13,6 +13,8 @@ namespace test
             string FirstName = "Veaceslav";
             Console.WriteLine("Buna ziua");
             Console.WriteLine("{0}", FirstName);
+            Console.WriteLine("Bine ati venit la Amdaris");
+            Console.WriteLine("Va uram mult succes");
         }
     }
 }
